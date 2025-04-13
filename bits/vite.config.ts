6917c252dc8745137819fdf82c8de614aac5f107
Arya <aryatiwari3101@gmail.com>
2025-04-14 03:24:00 +0530

    upload

--- bits/vite.config.ts
+++ bits/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bits/',  // this is crucial for proper routing in subfolders
+  base: './',  // this is crucial for proper routing in subfolders
   plugins: [react()],
 })
