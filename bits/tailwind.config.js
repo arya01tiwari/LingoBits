e1a4a40bad429c34309ea2e39c394f87d594581b
Arya <aryatiwari3101@gmail.com>
2025-04-14 03:36:20 +0530

    upload

--- bits/tailwind.config.js
+++ bits/tailwind.config.js
@@ -1,8 +1,8 @@
 export default {
   darkMode: "class", // Important for toggle-based dark mode
   content: [
-    "./bits/index.html",
-    "./bits/src/**/*.{js,ts,jsx,tsx}",
+    "./bits/index.html",  // For index.html in the bits folder
+    "./bits/src/**/*.{js,ts,jsx,tsx}",  // For React components in the bits/src folder
   ],
   theme: {
     extend: {
@@ -13,4 +13,5 @@ export default {
   },
   plugins: [],
 }
+
 
