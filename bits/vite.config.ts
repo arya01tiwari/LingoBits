9e0805c000683ed104faaea3e2adf6be00b6337f
Arya <aryatiwari3101@gmail.com>
2025-04-20 19:15:24 +0530

    up

--- bits/vite.config.ts
+++ bits/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', // Ensures assets are correctly referenced
+  base: '/', // Ensures assets are correctly referenced
   plugins: [react()],
 });
